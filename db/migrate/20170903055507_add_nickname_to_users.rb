c88539a65e0ba1b6eb6ae946ad775c0a8e0532d4
ttomoki <takewarr1185@gmail.com>
2017-09-08 15:37:59 +0900

    Indexつけました！

--- db/migrate/20170903055507_add_nickname_to_users.rb
+++ db/migrate/20170903055507_add_nickname_to_users.rb
@@ -1,5 +1,6 @@
 class AddNicknameToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :name, :string
+    add_index :users, :name, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170903055507) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
