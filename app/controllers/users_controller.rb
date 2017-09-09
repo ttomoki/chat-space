7808f17872dfca2f04356ec3d6eec3aad403af6d
ttomoki <takewarr1185@gmail.com>
2017-09-09 13:30:19 +0900

    条件分岐つけました
    Editに戻った後にupdateできないです、、
    やり方ミスってますか？？

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,13 @@ class UsersController < ApplicationController
 	end
 
 	def update
-      current_user.update(update_params)
-      redirect_to :root
+	  if
+        current_user.update(update_params)
+        redirect_to :root
+      else
+      	binding.pry
+    	redirect_to 'edit_user_path(current_user)'
+      end
     end
 
     private
