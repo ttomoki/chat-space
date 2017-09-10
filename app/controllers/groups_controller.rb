8023541b2a2abd620f87022e41f910e60451703f
ttomoki <takewarr1185@gmail.com>
2017-09-10 21:13:12 +0900

    groups_create
    # what
    Groupsのコントローラー、モデル、ルーティング作成
    # why
    グルーピ機能実装の準備

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,22 @@
+class GroupsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
+  private
+    def create_params
+    end
+
+    def update_params
+    end
+
+end
