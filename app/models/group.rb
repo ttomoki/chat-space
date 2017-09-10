bf784cad91b6b5e89775ea2d87db8852d52f8ca9
ttomoki <takewarr1185@gmail.com>
2017-09-10 22:34:14 +0900

    中間テーブル、モデルの内容

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,13 @@
 class Group < ApplicationRecord
+  #validation
+  validates_presence_of :group_name
+  validates :group_name, uniqueness: true
+
+  #association
+  has_many :users through: :group_users
+  has_many :group_users
+  has_many :messages
+
+  accepts_nested_attributes_for :group_users
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  #validation
   validates_presence_of :name, :email
   validates :name, uniqueness: true
   validates :email, uniqueness: true
+
+  #association
+  # has_many :groups through: :group_users
+  # has_many :group_users
+  has_many :messages
 end
