1327a6b3da50114045d917a57b83a09aba7d71e6
ttomoki <takewarr1185@gmail.com>
2017-09-14 21:22:41 +0900

    Massage機能追加
    # what
    Message機能でimage以外送信できるようにした
    # why
    グループ間でmessageできるように

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,18 @@
 class MessagesController < ApplicationController
 	def index
+	  @groups = current_user.groups
+	  @group = Group.find(params[:group_id])
+      @message = Message.new
 	end
+
+	def create
+		binding.pry
+		Message.create(create_params)
+		redirect_to :root
+	end
+
+	private
+	  def create_params
+	  	params.require(:message).permit(:body,:image).merge(group_id: params[:group_id], user_id: current_user.id)
+	  end
 end
