402dfe206dca66bc97ea4aa414388b662e2c55df
ttomoki <takewarr1185@gmail.com>
2017-09-18 19:17:50 +0900

    修正spec,validatesつけました
    created_atのとこはけすとやっぱり出ないですね、、、。

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,13 +1,10 @@
 FactoryGirl.define do
 
   factory :message do
-  	id "1"
-  	user_id  "1"
-  	group_id  "1"
-    body  "abe"
+  	user_id 1
+  	group_id 1
+    body  Faker::Lorem.sentence
     image Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/matsuoka3.jpg'))
-    created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
-    updated_at { Faker::Time.between(2.days.ago, Time.now, :all) }
   end
 
 end
