ce6e6b3eb4231b33fb749cf777a1852bd05a3075
ttomoki <takewarr1185@gmail.com>
2017-09-20 12:22:45 +0900

    カラムをnilにしました

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -14,17 +14,17 @@ describe Message do
       expect(message).to be_valid
     end
     it "is invalid without body and image" do
-      message = build(:message, body: "", image: "")
+      message = build(:message, body: nil, image: nil)
       message.valid?
       expect(message.errors[:body_or_image]).to include("を入力してください")
     end
     it "is invalid without group_id" do
-      message = build(:message, group_id: "")
+      message = build(:message, group_id: nil)
       message.valid?
       expect(message.errors[:group_id]).to include("を入力してください")
     end
     it "is invalid without user_id" do
-      message = build(:message, user_id: "")
+      message = build(:message, user_id: nil)
       message.valid?
       expect(message.errors[:user_id]).to include("を入力してください")
     end
