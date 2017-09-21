67ec8c29bfc9991e50e31b8707316920e6d89910
ttomoki <takewarr1185@gmail.com>
2017-09-22 00:40:29 +0900

    Flashのテスト、中間テーブル作成

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 describe MessagesController, type: :controller do
   let(:user) { create(:user) }
-  let(:group) { create(:group) }
-  let(:message) { create(:message, group_id: group.id, user_id: user.id) }
-  let(:messages) { create_list(:message, 5, user_id: user.id, group_id: group.id) }
-
+  let(:messages) { user.messages }
+  let(:message) { user.messages.first }
+  let(:group) { user.groups.first }
   describe 'GET #index' do
 
     context 'user signed_in' do
@@ -44,20 +43,25 @@ describe MessagesController, type: :controller do
       	login_user user
       end
 
-      it "saves the new message" do
+      it "saves new message" do
         expect{ post :create, params: {message: attributes_for(:message), group_id: group.id}}.to change(Message, :count).by(1)
       end
 
-      it "can't save the new message" do
+      it "can't save new message" do
         expect{ post :create, params: {message: attributes_for(:message, body: "", image: ""), group_id: group.id}}.to change(Message, :count).by(0)
       end
 
-      it "save the message redirects" do
+      it "when can't save new message flash[:alert]" do
+        post :create, params: {message: attributes_for(:message, body: "", image: ""), group_id: group.id}
+        expect(flash[:alert]).not_to be_empty
+      end
+
+      it "save message redirects" do
         post :create, params: {message: attributes_for(:message), group_id: group.id}
         expect(response).to redirect_to group_messages_path
       end
 
-      it "can't save the message render" do
+      it "can't save message render" do
         post :create, params: {message: attributes_for(:message,body: "", image: ""), group_id: group.id}
         expect(response).to render_template :index
       end
--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,8 +3,6 @@ FactoryGirl.define do
   factory :message do
     body  Faker::Lorem.sentence
     image Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/matsuoka3.jpg'))
-    association :user, factory: :user
-    association :group, factory: :group
   end
 
 end
