2d737e6703190f000e5030038b4dd2a7a92af7b8
ttomoki <takewarr1185@gmail.com>
2017-10-01 20:14:30 +0900

    インクリメンタルサーチ
    # what
    インクリメンタルサーチの実装
    # why
    非同期でグループ作成時にユーザーをサーチするため
    # 補足
    追加した後に、もう一度文字を打つと二回追加できてしまう。
    最初からcurrent_userをチャットメンバーにいれられていない。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def search
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+     format.json
+    end
+  end
+
   private
     def update_params
       params.require(:user).permit(:name, :email)
